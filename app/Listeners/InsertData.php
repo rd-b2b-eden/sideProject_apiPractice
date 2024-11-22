ba2b6ea151069bb901cfd351c549e67435ec5b97
eden.chen <eden.chen@eden.chenMacBook-Pro>
2024-11-22 17:33:24 +0800

    移除無用construct

--- app/Listeners/InsertData.php
+++ app/Listeners/InsertData.php
@@ -10,16 +10,6 @@ use Illuminate\Queue\InteractsWithQueue;
 class InsertData implements ShouldQueue
 {
     /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      *
      * @param \App\Events\DataGot $event
