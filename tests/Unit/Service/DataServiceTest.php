fa4d155e07a404547d42c739107dc35addb6a2e5
eden.chen <eden.chen@eden.chenMacBook-Pro>
2024-11-27 16:46:00 +0800

    [fix] 移除無用test，修改Redis預期結果

--- tests/Unit/Service/DataServiceTest.php
+++ tests/Unit/Service/DataServiceTest.php
@@ -3,45 +3,25 @@
 namespace Tests\Unit\Service;
 
 use App\Service\DataService;
-use Mockery\MockInterface;
+use Illuminate\Support\Facades\Redis;
 use Tests\TestCase;
 
 class DataServiceTest extends TestCase
 {
-    private MockInterface $dataServiceMock;
-
-    private object $dataService;
+    private DataService $dataService;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->dataServiceMock = $this->mock(DataService::class);
-        $this->dataService = $this->instance(DataService::class, $this->dataServiceMock);
-    }
-
-    public function testDataServiceCreateDataSuccessfully()
-    {
-        // Act
-        // Assert
-        $this->dataServiceMock->shouldReceive('createData')
-            ->once()
-            ->with(3)
-            ->andReturnNull();
-        $this->dataService->createData(3);
+        $this->dataService = app(DataService::class);
     }
 
     public function testDataServiceCreateQueueSuccessfully()
     {
         // Act
         // Assert
-        $this->dataServiceMock->shouldReceive('createQueue')
-            ->once()
-            ->with(3)->andReturn([
-                [
-                    'status' => '0001',
-                    'description' => '[command] 資料產生中，將產生3筆',
-                ]
-            ]);
+        Redis::shouldReceive('connection')->andReturnSelf();
+        Redis::shouldReceive('set')->once();
         $this->dataService->createQueue(3);
     }
 }
